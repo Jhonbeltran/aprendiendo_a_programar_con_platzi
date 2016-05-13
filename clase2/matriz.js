096a3ff91f7eafe758d62fd2944e2e4f5227b8c5
Jhonbeltran <jbeltranleon@gmail.com>
2016-05-12 20:52:00 -0500

    El codigo de campo minado ahora es un juego con dios (aleatorio)

--- clase2/matriz.js
+++ clase2/matriz.js
@@ -10,11 +10,11 @@
 function explosion()
 {
 	alert("BOOM!!");
-	document.write("<h1>BOOM! Elegiste un area minada :(</h1>");
+	document.write("<h1>BOOM! ¿Donde está tu Dios ahora?</h1>");
 }
 function ganaste()
 {
-	document.write("Eres un ganador :)");	
+	document.write("Puedes seguir siendo pecaminoso");	
 }
 
 //1 = Bomba. 0 = No hay bomba
@@ -26,29 +26,36 @@ var textos = ["Cesped", "Bomba"];
 
 var x, y;
 
-alert("Estás en un campo minado\n" +
-	  "Elije una posición entre el 0 y el 2 para X y para Y");
+// alert("Estás en un campo minado\n" +
+// 	  "Elije una posición entre el 0 y el 2 para X y para Y");
 
 
-//Con esto hice que la eleccion de la coordenada y sea aleatoria
-function seleccionMaquina(minimo, maximo)
+alert("Estas en un campo minado y acabas de dar un paso\n¿Pisaste una bomba o es tu día de suerte?");
+
+//Con esto hice que la eleccion de la coordenada x |y| y sean random
+function seleccionMaquinaEjeX(minimo, maximo)
 {
 	var numero = Math.floor( Math.random() * (maximo - minimo + 1) + minimo );
 	return numero;
 }
+function seleccionMaquinaEjeY(minimo, maximo)
+{
+	var numero = Math.floor( Math.random() * (maximo - minimo + 1) + minimo );
+	return numero;
+}
+var opcionMaquinaEjeX = seleccionMaquinaEjeX(0,2);
+var opcionMaquinaEjeY = seleccionMaquinaEjeY(0,2);
 
-var opcionMaquina = seleccionMaquina(0,2);
-
-
-x = prompt("Posición en X? (entre 0 y 2)");
+//x = prompt("Posición en X? (entre 0 y 2)");
 //Aleatoria
-y = opcionMaquina;
+x = opcionMaquinaEjeX;
+y = opcionMaquinaEjeY;
 
 
 if(x <= 2 && y <= 2)
 {
 	var posicion = campo[x][y];
-	document.write("Elegiste " + textos[posicion] + "<br />");
+	document.write("Los dados de Dios te arrastraron a un lugar con " + textos[posicion] + "<br />");
 	if(posicion == 1)
 	{
 		explosion();
