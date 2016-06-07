bba120f0b6e4a913b61b9aa62fab4cde12554f4b
Jhonbeltran <jbeltranleon@gmail.com>
2016-06-07 00:41:00 -0500

    En el juego de ahorcado ya nos dice cuando ganamos

--- Prototype/ahorcado/ahorcado.js
+++ Prototype/ahorcado/ahorcado.js
@@ -10,7 +10,7 @@ function seleccionPalabra(minimo, maximo)
 var opciones = ["Guardia", "Snow", "Stark", "Aria", "Olly", "Ned", "Kingslanding"]
 var opcionMaquina = seleccionPalabra(0,3);
 var palabra = opciones[opcionMaquina];
-var hombre, l, espacio;
+var hombre, l, b, espacio;
 
 //Declaración de la clase Ahorcado (recibe como parametro contexto)
 /*var Ahorcado = function (con)*/
@@ -170,7 +170,7 @@ Ahorcado.prototype.trazar = function ()
 function iniciar () 
 {
 	l = document.getElementById("letra");
-	var b = document.getElementById("boton");
+	b = document.getElementById("boton");
 	/*var canvas = document.getElementById("canvas");
 	//podemos darle un ancho y un alto a la etiqueta de canvas desde js
 	canvas.width = 500;
@@ -218,12 +218,14 @@ function mostrarPalabra(palabra, hombre, letra)
 		{
 			espacio[p] = letra;
 			encontrado = true;
-		}
+		
+	}
 	}
 	mostrarPista(espacio);
-	/*if (mostrarPista(espacio)==mostrarPista(palabra)) {
-		alert("Ganaste");
-	}*/
+	if ((espacio.toString().replace(/,/g, ""))==palabra) {
+		alert("Ganaste")
+		b.disabled= true;
+	}
 
 	//Si NO lo encontré
 	if(!encontrado)
